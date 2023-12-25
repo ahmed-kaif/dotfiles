6860e500b7a72777a38cbf39598d047f327cdd2d
ahmed-kaif <kaifkhan.rcs@gmail.com>
2023-12-25 14:37:47 +0600

    nvim config

---
+++ nvim/.config/nvim/lua/custom/plugins/init.lua
@@ -0,0 +1,75 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+return {
+  {
+    'goolord/alpha-nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons',
+      'BlakeJC94/alpha-nvim-fortune' },
+    config = function ()
+      require'alpha'.setup(require'alpha.themes.dashboard'.config)
+    end
+  },
+  {
+    "nvim-neorg/neorg",
+    build = ":Neorg sync-parsers",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("neorg").setup {
+        load = {
+          ["core.defaults"] = {}, -- Loads default behaviour
+          ["core.concealer"] = {}, -- Adds pretty icons to your documents
+          ["core.dirman"] = { -- Manages Neorg workspaces
+            config = {
+              workspaces = {
+                notes = "~/notes",
+              },
+            },
+          },
+        },
+      }
+    end,
+  },
+  { "CRAG666/code_runner.nvim",
+    config = function()
+      require('code_runner').setup({
+        filetype = {
+          java = {
+            "cd $dir &&",
+            "javac $fileName &&",
+            "java $fileNameWithoutExt"
+          },
+          cpp = {
+            "cd $dir &&",
+            "g++ $fileName -o $fileNameWithoutExt &&",
+            "$dir/$fileNameWithoutExt"
+          },
+          python = "python3 -u",
+          typescript = "deno run",
+          rust = {
+            "cd $dir &&",
+            "rustc $fileName &&",
+            "$dir/$fileNameWithoutExt"
+          },
+        },
+      })
+    end,
+  },
+  {'akinsho/bufferline.nvim', version = "*", dependencies = 'nvim-tree/nvim-web-devicons',
+    after="catppuccin",
+    config = function ()
+      require("bufferline").setup{
+        options = {
+          numbers = function (options)
+            return string.format('%s', options.raise(options.ordinal))
+          end,
+        },
+        highlights = require("catppuccin.groups.integrations.bufferline").get()
+      }
+    end
+  },
+  {
+    'frabjous/knap';
+  },
+}
