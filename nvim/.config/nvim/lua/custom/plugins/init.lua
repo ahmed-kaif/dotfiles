62a1223a653c2531886925a74826d9c15fe6ada3
ahmed-kaif <kaifkhan.rcs@gmail.com>
2024-04-15 23:30:21 +0600

    nvim silicon plugin added

--- nvim/.config/nvim/lua/custom/plugins/init.lua
+++ nvim/.config/nvim/lua/custom/plugins/init.lua
@@ -72,4 +72,23 @@ return {
   {
     'frabjous/knap';
   },
+  {
+    "michaelrommel/nvim-silicon",
+    lazy = true,
+    cmd = "Silicon",
+    config = function()
+      require("silicon").setup({
+        -- Configuration here, or leave empty to use defaults
+        font = "JetBrainsMono Nerd Font=34;Noto Color Emoji=34",
+        theme = "Monokai Extended",
+        to_clipboard = true,
+        window_title = function()
+          return vim.fn.fnamemodify(
+            vim.api.nvim_buf_get_name(vim.api.nvim_get_current_buf()),
+            ":t"
+          )
+        end,
+      })
+    end
+  },
 }
