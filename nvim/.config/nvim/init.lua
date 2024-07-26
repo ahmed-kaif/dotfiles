63c59d5656bc7ef0d986ce4f0d39a53667b90bb5
ahmed-kaif <kaifkhan.rcs@gmail.com>
2024-07-26 14:04:26 +0600

    nvim keymap added

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -387,6 +387,7 @@ vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = tr
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 vim.keymap.set('i', 'jj', "<ESC>")
 vim.keymap.set('i', 'jk', "<ESC>")
+vim.keymap.set('n', 'Q', "gqq")
 
 -- Diagnostic keymaps
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
