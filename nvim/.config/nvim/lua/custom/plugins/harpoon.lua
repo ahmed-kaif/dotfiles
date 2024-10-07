2ef99acebda13776b1f6a07853f1795c9d5f464b
ahmed-kaif <kaifkhan.rcs@gmail.com>
2024-10-07 22:14:25 +0600

    which key | harpoon options

--- nvim/.config/nvim/lua/custom/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/custom/plugins/harpoon.lua
@@ -5,13 +5,13 @@ local harpoon = require("harpoon")
 harpoon:setup()
 -- REQUIRED
 
-vim.keymap.set("n", "<leader>ha", function() harpoon:list():add() end)
-vim.keymap.set("n", "<leader>hh", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+vim.keymap.set("n", "<leader>ha", function() harpoon:list():add() end, { desc = 'Add buffer to Harpoon' })
+vim.keymap.set("n", "<leader>hh", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = 'Harpoon List' })
 
-vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
-vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
-vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
-vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
+vim.keymap.set('n', '<leader>1', function() harpoon:list():select(1) end, { desc = 'Select Harpoon 1' })
+vim.keymap.set('n', '<leader>2', function() harpoon:list():select(2) end, { desc = 'Select Harpoon 2' })
+vim.keymap.set('n', '<leader>3', function() harpoon:list():select(3) end, { desc = 'Select Harpoon 3' })
+vim.keymap.set('n', '<leader>4', function() harpoon:list():select(4) end, { desc = 'Select Harpoon 4' })
 
 -- Toggle previous & next buffers stored within Harpoon list
 vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
