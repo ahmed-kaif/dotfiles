f7de0eab01229c63fdedf9eebbf3b793183cc99f
ahmed-kaif <kaifkhan.rcs@gmail.com>
2025-10-27 13:02:16 +0600

    updated few plugins and added code_runner plugin

--- LazyVim/.config/LazyVim/lua/config/keymaps.lua
+++ LazyVim/.config/LazyVim/lua/config/keymaps.lua
@@ -5,3 +5,10 @@ local set = vim.keymap.set
 
 set("n", "<localleader>pi", "<CMD>Pioinit<CR>")
 set("n", "<localleader>pr", "<CMD>Piorun<CR>")
+set("n", "<leader>rr", ":RunCode<CR>", { noremap = true, silent = false })
+set("n", "<leader>rf", ":RunFile<CR>", { noremap = true, silent = false })
+set("n", "<leader>rft", ":RunFile tab<CR>", { noremap = true, silent = false })
+set("n", "<leader>rp", ":RunProject<CR>", { noremap = true, silent = false })
+set("n", "<leader>rc", ":RunClose<CR>", { noremap = true, silent = false })
+-- set("n", "<leader>crf", ":CRFiletype<CR>", { noremap = true, silent = false })
+-- set("n", "<leader>crp", ":CRProjects<CR>", { noremap = true, silent = false })
--- LazyVim/.config/LazyVim/lua/plugins/colorscheme.lua
+++ LazyVim/.config/LazyVim/lua/plugins/colorscheme.lua
@@ -56,7 +56,7 @@ return {
       },
     },
   },
-  -- Configure LazyVim to load gruvbox
+  -- Configure LazyVim to load catppuccin
   {
     "LazyVim/LazyVim",
     opts = {
